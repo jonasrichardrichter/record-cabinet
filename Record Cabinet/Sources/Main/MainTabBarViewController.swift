48dfd8aea3f0609c23a7b64c559053e88c434dc2
Jonas Richard Richter <kontakt@jonasrichter.eu>
2022-05-07 17:02:37 +0200

    Add artist page to tab bar

--- Record Cabinet/Sources/Main/MainTabBarViewController.swift
+++ Record Cabinet/Sources/Main/MainTabBarViewController.swift
@@ -15,10 +15,13 @@ class MainTabBarViewController: UITabBarController {
         let recordsVC = UINavigationController(rootViewController: LibraryCollectionViewController())
         recordsVC.tabBarItem = UITabBarItem(title: "RECORDS_COLLECTION_TITLE".localized(), image: UIImage(systemName: "square.stack.fill"), selectedImage: UIImage(systemName: "square.stack.fill"))
         
+        let artistVC = UINavigationController(rootViewController: ArtistTableViewController())
+        artistVC.tabBarItem = UITabBarItem(title: "ARTISTS_TITLE".localized(), image: UIImage(systemName: "music.mic"), selectedImage: UIImage(systemName: "music.mic"))
+        
         let searchVC = UINavigationController(rootViewController: SearchViewController())
         searchVC.tabBarItem = UITabBarItem(title: "SEARCH_TITLE".localized(), image: UIImage(systemName: "magnifyingglass"), selectedImage: UIImage(systemName: "magnifyingglass"))
         
-        self.viewControllers = [recordsVC, searchVC]
+        self.viewControllers = [recordsVC, artistVC, searchVC]
         self.tabBar.tintColor = UIColor(named: "AccentColor")
     }
 }
